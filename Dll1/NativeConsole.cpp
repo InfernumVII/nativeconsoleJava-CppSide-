3b9eb1b9e3a79a81093ffa1d19434e65871c3745
InfernumVII <egor554shved@gmail.com>
2025-04-26 07:52:22 +0300

    Добавьте файлы проекта.

---
+++ Dll1/NativeConsole.cpp
@@ -0,0 +1,12 @@
+﻿#include <jni.h>
+#include <windows.h>
+#include <conio.h>  //For _kbhit and _getch
+#include "NativeConsole.h"
+
+
+JNIEXPORT jint JNICALL Java_com_infernumvii_NativeConsole_readCharImmediately(JNIEnv* env, jclass cls) {
+    if (_kbhit()) { //Non-blocking check if key is available
+        return _getch(); //Get character without echo
+    }
+    return -1;
+}
